d1c2f9424105d1b8d5ed3cc43613482f42cca461
rohanbhore <rohan@gmail.com>
2017-02-01 16:29:17 +0530

    second commit

--- app.js
+++ app.js
@@ -6,10 +6,34 @@
 
       CounterController.$inject = ['$scope'];
 
-      function CounterController($scope){
+      function CounterController($scope)
+      {
+        $scope.onceCounter =0;
+        $scope.counter=0;
+        $scope.name="Rohan";
         $scope.showNumberOfWatchers=function()
         {
-          console.log($scope);
+          console.log("#no of Watchers :", $scope.$$watchersCount);
         };
+        $scope.countOnce=function()
+        {
+          $scope.onceCounter=1;
+        };
+        $scope.upCounter=function()
+        {
+          $scope.counter++;
+        }
+        $scope.$watch(function()
+        {
+        console.log("Digest Loop Fired!");
+        })
+        // $scope.$watch('onceCounter',function(newValue,oldValue){
+        //   console.log("old value :",oldValue);
+        //   console.log("new value :",newValue);
+        // });
+        // $scope.$watch('counter',function(newValue,oldValue){
+        //   console.log("Counter old value :",oldValue);
+        //   console.log("Counter new value :",newValue);
+        // });
       }
 })();
